6242e2a84b087218eddc36008238c0f6ee0d7b10
오지원 <64193469+ojw4184@users.noreply.github.com>
2021-08-31 14:14:05 +0900

    프론트 작업중

--- src/allthatbook/mvc/view/CartMenuView.java
+++ src/allthatbook/mvc/view/CartMenuView.java
@@ -56,14 +56,16 @@ public class CartMenuView {
 
 			switch(Integer.parseInt(sc.nextLine())) {
 			case 1:
-				System.out.println(" < " + book.getBookName() + " > 책을 대여하시겠습니까? ( 네 or 아니오 )");
+				System.out.println(book.getBookNo() + ". < " + book.getBookName() + " > 책을 대여하시겠습니까? ( 네 or 아니오 )");
 				String checkRental = sc.nextLine();
 				if("네".equals(checkRental)) {
 					RentalController.insertRental(user, book);
 				}else if("아니오".equals(checkRental)) {
+					System.out.println("대출되지 않았습니다.");
 					return;
 				}else {
 					System.out.println("( 네 or 아니오 ) 중 하나만 입력해주세요.");
+					CartMenuView.printCartMenu(user, book);
 				}
 				
 				return;
@@ -87,6 +89,34 @@ public class CartMenuView {
 	public static void printCartMenu(User user) {
 		while(true) {
 			System.out.println("1.대여하기  |  2.장바구니 담기  |  3.장바구니 보기  |  4.돌아가기  ");
+			System.out.print("번호 입력 > ");
+			switch(Integer.parseInt(sc.nextLine())) {
+			case 1:
+				System.out.print("대여 하실책 번호 입력 > ");
+				int bookNo = Integer.parseInt(sc.nextLine());
+				RentalController.insertRental(user, bookNo);
+				break;
+			case 2:
+				CartMenuView.putCart(user.getUserId());
+				break;
+			case 3:		/////////////////////////
+				CartMenuView.viewCart(user.getUserId());
+				SessionSet ss = SessionSet.getInstance();
+				Session session = ss.get(user.getUserId());
+
+				Cart cart = (Cart) session.getAttribute("cart");
+				CartController.rentalCartBook( user.getUserId(), cart);
+				//세션에 있는 cart객체 안에 있는 cartDetailList 안에 번호에 맞는 삭제
+				break;
+			case 4:
+				return;
+			}
+		}
+	}
+	
+	public static void printRentalMenu(User user) {
+		while(true) {
+			System.out.println("1.전체 목록 대여하기  |  4.돌아가기  ");
 			System.out.print("번호 입력 > ");
 			switch(Integer.parseInt(sc.nextLine())) {
 			case 1:
